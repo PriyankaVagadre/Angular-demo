85edf7f139bd32a345e05fc4ce9d04b1ca07afa2
Priyanka Vagadre <priyanka.vagadre@EXXATSYSTEMS.COM>
2019-06-24 18:56:27 +0530

    recipe model

--- src/app/new-project/recipes/recipe-list/recipe-list.component.ts
+++ src/app/new-project/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Recipe} from '../recipe.model'
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
-
+  public recipes : Recipe[] =[];
   constructor() { }
 
   ngOnInit() {
