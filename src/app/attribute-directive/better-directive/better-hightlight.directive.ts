ac48120e70c4a66fb1a50b1e44386b7bae5e152b
Priyanka Vagadre <priyanka.vagadre@EXXATSYSTEMS.COM>
2019-08-01 15:35:16 +0530

    @HostListener for attribute directive

--- src/app/attribute-directive/better-directive/better-hightlight.directive.ts
+++ src/app/attribute-directive/better-directive/better-hightlight.directive.ts
@@ -1,4 +1,5 @@
-import { Directive, OnInit , Renderer2, ElementRef } from '@angular/core';
+import { Directive, OnInit , Renderer2, ElementRef, HostListener } from '@angular/core';
+import { MockNgModuleResolver } from '@angular/compiler/testing';
 
 @Directive({
   selector: '[appBetterHightlight]'
@@ -11,5 +12,17 @@ export class BetterHightlightDirective implements OnInit {
    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue'); //4 agruments are there but you can pass 3 also
                           // the native element    // property for style // value of the style property
    this.renderer.setStyle(this.elRef.nativeElement, 'color', '#fff');
+  }
+
+  @HostListener('mouseenter') mouseover(evnetData : Event){
+    this.renderer.setStyle(this.elRef.nativeElement , 'background-color', 'yellow');
+    this.renderer.setStyle(this.elRef.nativeElement, 'color', '#000');
+    this.renderer.setStyle(this.elRef.nativeElement, 'border', '1px solid')
+  }
+
+  @HostListener('mouseleave') mouseleave(evnetData : Event){
+    this.renderer.setStyle(this.elRef.nativeElement , 'background-color', '#f2f2f2');
+    this.renderer.setStyle(this.elRef.nativeElement, 'color', '#000');
+    this.renderer.setStyle(this.elRef.nativeElement, 'border', '1px solid')
   }
 }
